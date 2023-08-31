da2e2d105dfff078f20aed80362c902229e96548
Luke Scott <77630341+Luke270601@users.noreply.github.com>
2023-08-31 17:01:11 +0100

    Added a redirect to the login submission will rework once database is introduced

--- client/src/Components/Login.jsx
+++ client/src/Components/Login.jsx
@@ -1,9 +1,26 @@
-import React, {useState} from 'react';
+import React, { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../CSS/App.css';
 
 const PromptPopup = () => {
   const [isOpen, setIsOpen] = useState(false);
-  const [userInput, setUserInput] = useState('');
+  const popupRef = useRef(null);
+  const navigate = useNavigate(); // Initialize useHistory
+
+
+  useEffect(() => {
+    const handleOutsideClick = (event) => {
+      if (popupRef.current && !popupRef.current.contains(event.target)) {
+        closePopup();
+      }
+    };
+
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+      document.removeEventListener('mousedown', handleOutsideClick);
+    };
+  }, []);
 
   const openPopup = () => {
     setIsOpen(true);
@@ -13,23 +30,22 @@ const PromptPopup = () => {
     setIsOpen(false);
   };
 
-  const handleInputChange = (event) => {
-    setUserInput(event.target.value);
-  };
 
   const handleSubmit = () => {
     // Process userInput as needed
-    console.log('User input:', userInput);
     closePopup();
+    navigate('/calender')
   };
 
   return (
-    <div className="popup-container">
-      <button className={"login"} onClick={openPopup}>Open Prompt</button>
-      <div className={`popup ${isOpen ? 'open' : ''}`}>
-        <span className="close" onClick={closePopup}>&times;</span>
-        <p>Enter your information:</p>
-        <input type="text" value={userInput} onChange={handleInputChange} placeholder="Your input" />
+    <div id="promptBox" className="popup-container">
+      <button className={"login"} onClick={openPopup}>Login</button>
+      <div ref={popupRef} className={`popup ${isOpen ? 'open' : ''}`}>
+        <li>Enter Login</li>
+        <li>Email:</li><input type="text" placeholder="Your input" />
+        <br/>
+        <li>Password:</li><input type="text" placeholder="Your input" />
+        <br/>
         <button onClick={handleSubmit}>Submit</button>
       </div>
     </div>

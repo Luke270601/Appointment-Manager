1398d0b5de7877e3872bfacfbe4d6d48992cf4e5
Luke Scott <77630341+Luke270601@users.noreply.github.com>
2023-09-04 21:11:24 +0100

    Placed login authentication is navbar and removed auth in homepage

--- client/src/Pages/Home-Page.jsx
+++ client/src/Pages/Home-Page.jsx
@@ -1,30 +1,9 @@
 import '../CSS/App.css';
 import { NavBar } from "../Components/NavBar";
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React from 'react';
+
 
 function HomePage() {
-    const [loggedIn, setLoggedIn] = useState(false);
-    const navigate = useNavigate(); // Initialize useHistory
-
-    useEffect(() => {
-        fetch("/login")
-          .then(response => response.json())
-          .then(data => {
-            if (data.loggedIn) {
-              setLoggedIn(true); // Correct the assignment here
-            }
-          })
-          .catch(error => {
-            console.error('Error:', error);
-          });
-      }, []);
-
-    useEffect(() => {
-        if (loggedIn) {
-          navigate("/calender")
-        }
-    }, [loggedIn, navigate]);
 
     return (
         <div className="home-page">
